35d615292578f650484da89fc9571071c5bd9707
zwackwandrey <zwackwandrey@cip.ifi.lmu.de>
2023-01-30 01:28:26 +0100

    fixed Map loading bug when trying to load the map from inside a JAR

--- src/main/java/Desperatedrosseln/Json/utils/JsonFileReader.java
+++ src/main/java/Desperatedrosseln/Json/utils/JsonFileReader.java
@@ -1,14 +1,12 @@
 package Desperatedrosseln.Json.utils;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class JsonFileReader {
 
-    public String readFile(String filePath) throws IOException {
+    public String readFile(InputStream filePath) throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(filePath));
         String line;
         while ((line = bufferedReader.readLine()) != null) {
             stringBuilder.append(line);
