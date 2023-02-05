cdfcd13559c53248b43691728f95b6161cb5e760
Manu <schneidermanu@cip.ifi.lmu.de>
2023-02-05 17:39:28 +0100

    Major UI rework: - new login and lobby scene with improved usability and appearance - added a view and the logic for screens that are to small

--- src/main/java/Desperatedrosseln/Main.java
+++ src/main/java/Desperatedrosseln/Main.java
@@ -4,20 +4,35 @@ import Desperatedrosseln.Local.Controllers.LoginController;
 import Desperatedrosseln.Local.Controllers.MainController;
 import Desperatedrosseln.Local.Controllers.LobbyController;
 
+import Desperatedrosseln.Local.Controllers.ScreenSizeErrorController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application implements Runnable {
 
-    // sceneNumber 0: mainScene, 1: loginScene, 2: lobbyScene
+    private final int minSupportedWidth = 1024;
+    private final int minSupportedHeight = 768;
+
+    // sceneNumber 0: mainScene, 1: loginScene, 2: lobbyScene, 3: screenSizeErrorScene
     @Override
     public void start(Stage primaryStage) {
+
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        double userScreenHeight = screenBounds.getHeight();
+        double userScreenWidth = screenBounds.getHeight();
+
         try {
-            launchScene(primaryStage, 1);
+            if (userScreenHeight < minSupportedHeight || userScreenWidth < minSupportedWidth) {
+                launchScene(primaryStage, 3);
+            } else {
+                launchScene(primaryStage, 1);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -33,6 +48,9 @@ public class Main extends Application implements Runnable {
         } else if (sceneNumber == 2) {
             LobbyController lobbyController = new LobbyController();
             lobbyController.startLobbyScene(stage);
+        } else if (sceneNumber == 3) {
+            ScreenSizeErrorController screenSizeErrorController = new ScreenSizeErrorController();
+            screenSizeErrorController.startScene(stage);
         }
     }
     @Override
